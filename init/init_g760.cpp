8283802fc9cc62d162bf497ea24f4ebc238e31ca
mparus <mikael.parus@gmail.com>
2016-09-24 20:59:31 +0200

    fix device name, switch to mpdecision, enable root and some other changes
    Change-Id: I9d5a7aa50f3d5dc9fac6db1c83e8720abf7b2e8f

--- init/init_g760.cpp
+++ init/init_g760.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2014, The Linux Foundation. All rights reserved.
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -12,6 +13,7 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
+
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -25,135 +27,114 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
-    char model[110];
-    FILE* fp;
+    std::ifstream fin;
+    std::string buf;
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    fp = fopen("/proc/app_info", "rb");
-    while (fgets(model, 100, fp))
-        if (strstr(model, "huawei_fac_product_name") != NULL)
+    fin.open("/proc/app_info");
+    while (getline(fin, buf))
+        if (buf.find("huawei_fac_product_name") != std::string::npos)
             break;
+    fin.close();
 
-    /* G620S-L01 */
-    if (strstr(model, "G620S-L01") != NULL) {
-        property_set("ro.product.model", "G620S-L01");
-        property_set("ro.product.name", "G620S-L01");
-        property_set("ro.product.device", "G620S-L01");
-        property_set("ro.build.product", "G620S-L01");
-        property_set("ro.telephony.default_network", "9");
-        property_set("ro.sf.lcd_density","320");
-        property_set("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
+    /* C8817D */
+    if (buf.find("C8817D") != std::string::npos) {
+        property_set("ro.product.model", "C8817D");
+        property_set("ro.product.device", "C8817D");
+        property_set("ro.build.product", "C8817D");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("ro.build.description", "C8817D-user 4.4.4 GRJ90 C92B381 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/C8817D/hwC8817D:4.4.4/HuaweiC8817D/C92B381:user/release-keys");
+    }
+    /* C8817E */
+    else if (buf.find("C8817E") != std::string::npos) {
+        property_set("ro.product.model", "C8817E");
+        property_set("ro.product.device", "C8817E");
+        property_set("ro.build.product", "C8817E");
+        property_set("ro.build.description", "C8817E-user 4.4.4 GRJ90 C92B380 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/C8817E/hwC8817E:4.4.4/HuaweiC8817E/C92B380:user/release-keys");
     }
     /* G621-TL00 */
-    else if (strstr(model, "G621-TL00") != NULL) {
+    else if (buf.find("G621-TL00") != std::string::npos) {
         property_set("ro.product.model", "G621-TL00");
-        property_set("ro.product.name", "G621-TL00");
         property_set("ro.product.device", "G621-TL00");
         property_set("ro.build.product", "G621-TL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "22");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
-        property_set("ro.sf.lcd_density","320");
         property_set("ro.build.description", "G621-TL00-user 4.4.4 GRJ90 C01B361SP01 release-keys");
         property_set("ro.build.fingerprint", "Honor/G621-TL00/hwG621-TL00:4.4.4/HonorG621-TL00/C01B361SP01:user/release-keys");
     }
-    /* G622-CL */
-    else if (strstr(model, "G622-CL") != NULL) {
-        property_set("ro.product.model", "G622-CL");
-        property_set("ro.product.name", "G622-CL");
-        property_set("ro.product.device", "G622-CL");
-        property_set("ro.build.product", "G622-CL");
+    /* G620S-UL00 */
+    else if (buf.find("G620S-UL") != std::string::npos) {
+        property_set("ro.product.model", "G620S-UL00");
+        property_set("ro.product.device", "G620S-UL00");
+        property_set("ro.build.product", "G620S-UL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "22");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
-        property_set("ro.sf.lcd_density","320");
+        property_set("ro.build.description", "G620S-UL00-user 4.4.4 GRJ90 C17B246 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/G620S-UL00/hwG620S-UL00:4.4.4/HuaweiG620S-UL00/C17B246:user/release-keys");
     }
-    /* G660S */
-    else if (strstr(model, "G660S") != NULL) {
-        property_set("ro.product.model", "G660S");
-        property_set("ro.product.name", "G660S");
-        property_set("ro.product.device", "G660S");
-        property_set("ro.build.product", "G660S");
+    /* G620S-L01 */
+    else if (buf.find("G620S-L01") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L01");
+        property_set("ro.product.device", "G620S-L01");
+        property_set("ro.build.product", "G620S-L01");
+        property_set("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
+    }
+    /* G620S-L02 */
+    else if (buf.find("G620S-L02") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L02");
+        property_set("ro.product.device", "G620S-L02");
+        property_set("ro.build.product", "G620S-L02");
+        property_set("ro.build.description", "G620S-L02-user 4.4.4 GRJ90 C00B380 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/G620S-L02/hwG620S-L02:4.4.4/HuaweiG620S-L02/C00B380:user/release-keys");
+    }
+    /* Che1-CL10 */
+    else if (buf.find("Che1-CL10") != std::string::npos) {
+        property_set("ro.product.model", "Che1-CL10");
+        property_set("ro.product.device", "Che1-CL10");
+        property_set("ro.build.product", "Che1-CL10");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "22");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
-        property_set("ro.sf.lcd_density","320");
+        property_set("ro.build.description", "Che1-CL10-user 4.4.4 GRJ90 C92B275 release-keys");
+        property_set("ro.build.fingerprint", "Honor/Che1-CL10/Che1:4.4.4/Che1-CL10/C92B275:user/ota-rel-keys,release-keys");
     }
-    /* G760-L01 */
-    else if (strstr(model, "G760-L01") != NULL) {
-        property_set("ro.product.model", "G760-L01");
-        property_set("ro.product.name", "G760-L01");
-        property_set("ro.product.device", "G760-L01");
-        property_set("ro.build.product", "G760-L01");
-        property_set("ro.telephony.default_network", "9");
-        property_set("ro.sf.lcd_density","320");
-        property_set("ro.build.description", "G7-L01-user 5.1.1 GRJ90 C432B331 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/G7-L01/hwG7-L01:5.1.1/HuaweiG7-L01/C432B331:user/release-keys");
-    }
-    /* Y550-L01 */
-    else if (strstr(model, "Y550-L01") != NULL) {
-        property_set("ro.product.model", "Y550-L01");
-        property_set("ro.product.name", "Y550-L01");
-        property_set("ro.product.device", "Y550-L01");
-        property_set("ro.build.product", "Y550-L01");
-        property_set("ro.telephony.default_network", "9");
-        property_set("ro.sf.lcd_density","240");
-    }
-    /* Y630-CL */
-    else if (strstr(model, "Y630-CL") != NULL) {
-        property_set("ro.product.model", "Y630-CL");
-        property_set("ro.product.name", "Y630-CL");
-        property_set("ro.product.device", "Y630-CL");
-        property_set("ro.build.product", "Y630-CL");
+    /* Che1-CL20 */
+    else if (buf.find("Che1-CL20") != std::string::npos) {
+        property_set("ro.product.model", "Che1-CL20");
+        property_set("ro.product.device", "Che1-CL20");
+        property_set("ro.build.product", "Che1-CL20");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "22");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.com.android.dataroaming","true");
-        property_set("ro.ril.multi_rat_capable","true");
-        property_set("ro.sf.lcd_density","240");
+        property_set("ro.build.description", "Che1-CL20-user 4.4.4 GRJ90 C00B275 release-keys");
+        property_set("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
     }
-    /* Y635-L01 */
-    else if (strstr(model, "Y635-L01") != NULL) {
-        property_set("ro.product.model", "Y635-L01");
-        property_set("ro.product.name", "Y635-L01");
-        property_set("ro.product.device", "Y635-L01");
-        property_set("ro.build.product", "Y635-L01");
-        property_set("ro.telephony.default_network", "9");
-        property_set("ro.sf.lcd_density","240");
+    /* Che1-L04 */
+    else if (buf.find("Che1-L04") != std::string::npos) {
+        property_set("ro.product.model", "Che1-L04");
+        property_set("ro.product.device", "Che1-L04");
+        property_set("ro.build.product", "Che1-L04");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("ro.build.description", "Che1-L04-user 4.4.4 GRJ90 C900B130 release-keys");
+        property_set("ro.build.fingerprint", "Honor/Che1-L04/Che1:4.4.4/Che1-L04/C900B130:user/ota-rel-keys,release-keys");
     }
    /* G760-L01 */
-    else if (strstr(model, "G760-L01") != NULL) {
+    else if (buf.find("G760-L01") != std::string::npos) {
         property_set("ro.product.model", "HUAWEI G760-L01");
         property_set("ro.product.name", "G760-L01");
         property_set("ro.product.device", "hwG760-L01");
@@ -161,5 +142,5 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.telephony.default_network", "20");
         property_set("ro.build.description", "G760-L01-user 5.1.1 GRJ90 C464B340 release-keys");
         property_set("ro.build.fingerprint", "Huawei/G760-L01/hwG760-L01:5.1.1/HuaweiG760-L01/C464B340:user/release-keys");
-}
+    }
 }
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_g760-user
+add_lunch_combo cm_g760-userdebug
